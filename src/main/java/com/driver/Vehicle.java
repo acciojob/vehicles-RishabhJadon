cc8db6bf5f13b0abee6ccdc7aa34bdd177af5203
Rishabh Singh <rishabhjadon976@gmail.com>
2023-11-03 18:03:29 +0530

    second commit

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -20,7 +20,9 @@ public class Vehicle {
     private int currentDirection;
 
     public Vehicle(String name) {
-
+       this.name = name;
+       currentSpeed = 0;
+       currentDirection= 0;
     }
 
     public void steer(int direction){
